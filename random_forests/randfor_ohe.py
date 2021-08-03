a5e73a0eaacf13915830c7554c7dc5ccfc3fb408
Mara <61852141+MaraTat@users.noreply.github.com>
2021-08-04 00:21:08 +0100

    Create randfor_ohe.py

---
+++ random_forests/randfor_ohe.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import make_column_transformer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.svm import SVC
+from sklearn.pipeline import make_pipeline
+from sklearn import metrics
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+######
+
+ohe = OneHotEncoder() # creating the onehotencoder for the data       
+X_train = trainpro.drop(['posmiddlePRO', 'omegaPRO', 'cistrans'], axis=1)
+X_traincols = [c for c in X_train.columns]
+X_train_enc = pd.DataFrame(ohe.fit_transform(X_train[X_traincols]).toarray())
+y_train = pd.get_dummies(trainpro['cistrans'], drop_first=True).rename(columns={'trans':'cistrans conformation'}) # creating dummies for cis trans conformation using the rename() method
+
+df = testpro
+X_test = df.drop(['posmiddlePRO', 'omegaPRO', 'cistrans'], axis=1)
+X_testcols = [c for c in X_test.columns]
+X_test_enc = pd.DataFrame(ohe.fit_transform(X_test[X_testcols]).toarray())
+y_test = pd.get_dummies(df['cistrans'], drop_first=True).rename(columns={'trans':'cistrans conformation'}) # creating dummies for cis trans conformation using the rename() method
+
+######
+
+rfc = RandomForestClassifier(n_estimators=100, oob_score=True, verbose=3)
+rfc.fit(X_train_enc, y_train.values.ravel())
+rfc_predictions = rfc.predict(X_test_enc)
+
+print(metrics.matthews_corrcoef(y_test, rfc_predictions).round(3))
+print(metrics.plot_confusion_matrix(rfc,X_test_enc,y_test, display_labels=['cis', 'trans'], cmap=plt.cm.Blues, normalize='true'))
+print(metrics.plot_roc_curve(rfc, X_test_enc, y_test))
+print(rfc.oob_score_.round(3))
+
