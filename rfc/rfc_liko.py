bd7e9cf5900d5a60482a1d258d5791fd9557b9eb
Mara <61852141+MaraTat@users.noreply.github.com>
2021-09-24 21:28:33 +0100

    Create rfc_liko.py

---
+++ rfc/rfc_liko.py
@@ -0,0 +1,60 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import make_column_transformer
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn import metrics
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
+import joblib
+
+allmixdfs21 = pd.read_pickle('D:/MSc_project/datapickles/allmixdfs21.pkl')
+
+##### preparing the encoding matrix #######
+likoehl = pd.read_csv('D:/MSc_project/LiKoehl.csv', sep=',', index_col=0)
+likoehl = likoehl.reset_index(drop=True)
+def liko_encode(seq):   
+    x = pd.DataFrame([likoehl[i] for i in seq]).reset_index(drop=True)  
+    m = x.values.flatten()
+    return m
+
+# the same process can be followed for any window size
+grid_mcc_rfc_liko = []
+grid_params_rfc_liko =[]
+
+for v in  allmixdfs21:
+    df = v
+    
+    X = df.drop(['posmiddlePRO', 'omegaPRO', 'cistrans'], axis=1)
+    Xcols = [c for c in X.columns]
+    y = pd.get_dummies(df['cistrans'], drop_first=True).rename(columns={'trans':'cistrans conformation'}) # creating dummies for cis trans conformation using the rename() method
+    X_enc = X[Xcols].apply(lambda k: pd.Series(liko_encode(k)),axis=1) # encoding the X using the LiKo matrix and function above
+    X_train, X_test, y_train, y_test = train_test_split(X_enc, y, test_size= 0.1)
+
+    rfc = RandomForestClassifier(oob_score=True, class_weight='balanced', verbose=3)
+    paramgrid = {'n_estimators': [100,200,500,1000]} # performing a grid search to find the best number of trees
+    grid = GridSearchCV(rfc, paramgrid , cv=5, verbose=3) # grid is the model, could be KNN or random forests
+    grid.fit(X_train, y_train.values.ravel()) # fitting the model
+    rfc_predictions = grid.predict(X_test) # getting the preds
+    
+    filename_rfc = 'D:/MSc_project/func_testing/new_sets/models/rfc_b62_cv5.sav' # saving the model
+    joblib.dump(grid, filename_rfc)
+    
+    grid_mcc_rfc_liko.append(metrics.matthews_corrcoef(y_test, rfc_predictions).round(3))
+    grid_params_rfc_liko.append(grid.best_params_)
+    
+    
+    print(metrics.plot_confusion_matrix(grid,X_test,y_test, display_labels=['cis', 'trans'], cmap=plt.cm.Blues, normalize='true'))
+    print(metrics.plot_roc_curve(grid, X_test, y_test))
+
+
+
+
+
+
+
+
+
